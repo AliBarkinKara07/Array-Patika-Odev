4f17fc1691f239b3675cab348723caf4db5ce756
AliBarkinKara07 <111884407+AliBarkinKara07@users.noreply.github.com>
2022-10-30 20:05:00 +0300

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = { 10, 11, 15, 90, 100 };
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            //////////////////////////////////
+            Console.WriteLine("Sıralı Diziler");
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+
+            ///////////////////////////////
+            Console.WriteLine("Clear Metodu");
+            Array.Clear(sayiDizisi, 2, 2);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+
+            }
+            //////////////////////////////
+            Console.WriteLine("Reverse Metodu");
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+
+            }
+            Console.WriteLine("Index of ");
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 100));
+
+            ////////////////////////////////
+            ///
+            Console.WriteLine("Resize");
+            Array.Resize<int>(ref sayiDizisi, 6);
+            sayiDizisi[5] = 99;
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+
+            }
+           
+
+        }
+    }
+}
